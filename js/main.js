1a5b30316f9a643cb40729f6040925dc2b65b6f5
jianfan0114@gmail.com <jianfan0114@gmail.com>
2020-06-21 16:23:12 +0800

    增加動畫效果

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
-$(".banner").slick();
-$(".brand").slick();
+AOS.init();
+
+$(".banner").slick({
+    autoplay:true,
+    autoplaySpeed:1500,
+    dots:true,
+    arrows: false
+    
+});
+$(".brand").slick({
+    slidesToShow:2,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    dots: true,
+    arrows: false
+});
